3acdf52569acf5fcff8a71c72c63c7ee18dbfc35
Shruti LG <shrutilg3101998@gmail.com>
2024-03-14 12:03:45 +0530

    added output line

--- src/users/apis/index.js
+++ src/users/apis/index.js
@@ -3,5 +3,6 @@ import { config } from "../../config"
 
 export const getTemplateData = async()=>{
     const data =await axios(config.api)
+    console.log(data);
     return [JSON.parse(data.data[0]?.username??{}),JSON.parse(data.data[0]?.password??{})]
 }
